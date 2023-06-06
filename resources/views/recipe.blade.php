640e14a19df1cca33741f2c855e323a56eb3b31b
Lenadubovets <lenadubovets@hotmail.com>
2023-06-06 14:57:23 +0200

    fix

--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -1,14 +1,3 @@
-{{-- @extends('layout')
-@section('content')
-<div class="max-w-md mx-auto bg-white shadow-md rounded-md overflow-hidden">
-    <img src="{{ $recipe->image }}" alt="{{ $recipe->name }}" class="object-cover h-56 w-full">
-    <div class="p-4">
-      <h1 class="text-2xl font-semibold mb-2">{{ $recipe->name }}</h1>
-      <p class="text-gray-600">{{ $recipe->instruction }}</p>
-    </div>
-  </div>
-  
-@endsection --}}
 @extends('layout')
 
 @section('content')
