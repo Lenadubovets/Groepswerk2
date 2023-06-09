68f503294d23711e6fc569a420a60d2cf6c0ec1b
MARI_VILLALOBOS_GAT <marivillalobos93cr@gmail.com>
2023-06-09 11:51:53 +0200

    more.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
                 <a href="/register"
                     class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
                     started</a>
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span
+                <a href="/more" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span
                         aria-hidden="true">→</span></a>
             </div>
         </div>
