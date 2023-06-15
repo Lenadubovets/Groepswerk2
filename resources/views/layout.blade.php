bb116e6ed4e0c814d1574b2529f7f1523c15d918
Lenadubovets <lenadubovets@hotmail.com>
2023-06-15 19:53:35 +0200

    fix flash message

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="//unpkg.com/alpinejs" defer></script>  
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- AlpineJS -->
     <script src="//unpkg.com/alpinejs" defer></script>
@@ -84,6 +85,7 @@
         </div>
         @include('footer')
     </div>
+    <x-flash-message/>
 </body>
 
 </html>
