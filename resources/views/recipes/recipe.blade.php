4b40c324b7fc215db1dacdbdf66934349c1adf0d
Zuzanna <zahorska.z96@gmail.com>
2023-06-25 14:02:12 +0200

    add styling to add button

--- resources/views/recipes/recipe.blade.php
+++ resources/views/recipes/recipe.blade.php
@@ -35,7 +35,7 @@
                             @endforeach
                             </ul>
                         </div>
-                        <button id="add-selected-btn">Add to Shopping List</button>
+                        <button class="bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 mt-10" data-tippy-content="Add Selected Ingredients to Shopping List" id="add-selected-btn">Add to Shopping List</button>
                     @else
                         <div class="p-6">
                             <p>No ingredients found.</p>
